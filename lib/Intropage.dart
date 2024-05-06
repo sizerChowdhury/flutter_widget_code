70c4770488bcf99081e2bbc4ca96dd1da4db672b
Shahin Reza <shahin.reza@brainstation-23.com>
2024-05-06 17:26:48 +0600

    Update Intropage and Play with the "Drawer" widget

--- lib/Intropage.dart
+++ lib/Intropage.dart
@@ -12,23 +12,44 @@ class IntroPage extends StatelessWidget {
             centerTitle: true,
             backgroundColor: Colors.grey[500],
           ),
-          body: Column(
-            children: [
-              ElevatedButton(
-                child: Text('Go to detailspage'),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/details');
-                },
+          drawer: Drawer(
+            child: Container(
+              color: Colors.grey[300],
+              child: ListView(
+                children: [
+                  DrawerHeader(
+                      child: Center(
+                    child: Text(
+                      'source code of Flutter Widget',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )),
+                  ListTile(
+                    leading: Icon(
+                      Icons.home,
+                    ),
+                    title: Text('Details page'),
+                    onTap: () {
+                      Navigator.pushNamed(context, '/details');
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(
+                      Icons.home,
+                    ),
+                    title: Text('TabPageSelector page'),
+                    onTap: () {
+                      Navigator.pushNamed(context, '/TabSelect');
+                    },
+                  )
+                ],
               ),
-              SizedBox(height: 20, width: 20),
-              ElevatedButton(
-                child: Text('Go to tabpaseselector'),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/TabSelect');
-                },
-              ),
-            ],
-          )),
+            ),
+          ),
+          body: Column(children: [])),
     );
   }
 }
